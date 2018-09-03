dd1324afd7b703c0e42661ecb0e4dada32a66a91
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-09-04 00:53:03 +0100

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -33,60 +33,6 @@ client.on('ready', () => { //console.log
             }
 });
 
-client.on('message', message => {
-var prefix = "-hi";
 
-  if (!message.content.startsWith(prefix)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id == 471788143637430273) return;
-
-
-if (message.content.startsWith(prefix + 'playing')) {
-  if (message.author.id !== '337629134371160065') return message.reply('**only for the botowner **')
-client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult}** : Playing changed to`)
-    message.react("ðŸ‘Œ")
-
-} else
-
-if (message.content.startsWith(prefix + 'setname')) {
-  if (message.author.id !== '471788143637430273') return message.reply('**only for the botowner **')
-  client.user.setUsername(argresult).then
-      message.channel.sendMessage(`**${argresult}** : done`)
-} else
-
-if (message.content.startsWith(prefix + 'stream')) {
-  if (message.author.id !== '471788143637430273') return message.reply('**only for the botowner **')
-client.user.setGame(argresult, "https://www.twitch.tv/0rzh0");
-    message.channel.sendMessage(`**${argresult}** streem changed to`)
-    message.react("ðŸ‘Œ")
-
-} else
-
-if (message.content.startsWith(prefix + 'watching')) {
-  if (message.author.id !== '471788143637430273') return message.reply('**only for the botowner **')
-    client.user.setActivity(argresult, {type : 'watching'});
- message.channel.sendMessage(`**${argresult}** : Watching changed to`)
- message.react("ðŸ‘Œ")
-
-} else
-	
-if (message.content.startsWith(prefix + 'listening')) {
-  if (message.author.id !== '471788143637430273') return message.reply('**only for the botowner **')
-client.user.setActivity(argresult);
-    message.channel.sendMessage(`**${argresult}** : listening changed to`)
-    message.react("ðŸ‘Œ")
-	
-} else
-
-if (message.content.startsWith(prefix + 'setavatar')) {
-if (message.author.id !== '471788143637430273') return message.reply('**only for the botowner **')
-client.user.setAvatar(argresult);
-    message.channel.sendMessage(`**${argresult}** :avatar changed to`);
-	message.react("ðŸ‘Œ")
-}
- });
- 
 
 client.login(process.env.BOT_TOKEN1);
